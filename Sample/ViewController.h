df8e59e9a0bb846ef3ad724d88dd5b9796650cf2
Jang Young-bin <jyb0823@gmail.com>
2015-09-10 10:54:34 +0900

    Initial Commit

---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Jang Young bin on 2015. 9. 10..
+//  Copyright (c) 2015년 Jang Young bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by Jang Young bin on 2015. 9. 10..
+//  Copyright (c) 2015년 Jang Young bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
